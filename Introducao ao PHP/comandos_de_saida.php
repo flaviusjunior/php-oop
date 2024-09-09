4fc242b771194386eb2918c6b6da7f8c0af34cc1
Flávio Júnior <flaviojunior9400@gmail.com>
2024-09-09 17:52:05 -0300

    Definindo variaves e algus tipos

--- Introducao ao PHP/comandos_de_saida.php
+++ Introducao ao PHP/comandos_de_saida.php
@@ -2,6 +2,21 @@
 echo 'a' . '<br>' . PHP_EOL;
 echo 'b' . '<br>' . PHP_EOL;
 
-//comando que imprime uma string no console
+//comando que imprime uma string no console.
 
 print 'abc';
+?>
+<?php
+//"var_dump" imprime conteudo de forma detalhada, muito comuns para realizar debug.
+$vetor = array ('Palio', 'Gol', 'Fiesta', 'Corsa');
+var_dump($vetor);
+
+//"print_r" imprime conteudo de forma detalhada, mas deixa o conteudo mais legivel.
+$vetor = array ('Palio', 'Gol', 'Fiesta', 'Corsa');
+print_r($vetor);
+
+//quebras de linhas no código.
+foreach ($vetor as $carro) {
+    echo $carro . "<br>";
+}
+?>
---
+++ Introducao ao PHP/variaveis.php
@@ -0,0 +1,52 @@
+<?php
+//tipos de variaveis.
+$nome = "João";
+$sobrenome = "Santos";
+
+echo "$sobrenome, $nome \n";
+
+// ou.
+
+//$codigo_cliente
+//$codigoCliente
+?>
+
+<?php
+// variavel variante.
+$variavel = 'nome';
+
+$$variavel = 'Maria';
+
+echo "$nome \n";
+
+?>
+
+<?php
+//uma variavele atribuida a outra, mas sem alteração após a atribuição.
+
+$a = 5;
+$b = $a;
+$b = 10;
+
+echo "$a\n"; //resultado 5.
+echo "$b\n"; //resultado 10.$a = 5;
+
+// com alterações pelo "&".
+$a = 5;
+$b = &$a; //aqui esta a alteração.
+$b = 10;
+
+echo "\n$a\n"; //resultado 10.
+echo "$b\n"; //resultado 10.
+
+?>
+<?php
+// já objetos são sempre copiados por referencia.
+
+$a = new stdClass; // o objato sendo criado.
+$a->nome = "Maria"; //definiu o atributo.
+$b = $a; // cria uma replica.
+$b->nome = "joana"; //definiu atributo novamente.
+print "\n$a->nome\n"; // resultado "Joana".
+print "$b->nome\n"; // resultado tmbm "joana.
+?>
