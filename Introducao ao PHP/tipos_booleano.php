b8666496782a4bbace4cdc64b393e80b99885fca
Flávio Júnior <flaviojunior9400@gmail.com>
2024-09-10 07:34:25 -0300

    Expressa valores logicos, verdadeiro ou falso

---
+++ Introducao ao PHP/tipos_booleano.php
@@ -0,0 +1,20 @@
+<?php
+
+$exibir_nome = true;
+
+if ($exibir_nome){
+    echo "José da Silva";
+} else {
+    echo "falso";
+}
+
+//outro exemplo.
+
+$umidade = 91;
+
+if ($umidade >= 90){
+    echo "Vai chover";
+} else{
+    echo "Nao vai chover";
+}
+?>
