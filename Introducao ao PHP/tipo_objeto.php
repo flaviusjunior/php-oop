3ad557f48e2f9cb2fb4351f46dad9fe545b4f168
Flávio Júnior <flaviojunior9400@gmail.com>
2024-09-11 09:55:41 -0300

    Um objeto é uma entidade com comportamento definido

---
+++ Introducao ao PHP/tipo_objeto.php
@@ -0,0 +1,14 @@
+<?php
+
+$carro = new stdClass;
+
+$carro-> modelo = 'GOl';
+$carro-> ano = '2007';
+$carro-> cor = 'Vermelho';
+
+print_r($carro);
+
+print $carro-> modelo;
+print $carro-> ano;
+print $carro-> cor;
+?>
