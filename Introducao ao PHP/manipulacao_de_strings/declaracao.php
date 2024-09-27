57411334cdb8297a404fb50c60eced5c5dc7a715
Flávio Júnior <flaviojunior9400@gmail.com>
2024-09-27 14:54:45 -0300

    A diferença é que todo conteúdo dentro das as  duplas é validado pelo PHP

---
+++ Introducao ao PHP/manipulacao_de_strings/declaracao.php
@@ -0,0 +1,19 @@
+<?php
+
+$fruta = 'maçã';
+
+print "como $fruta<br>" . PHP_EOL;
+print 'como $fruta<br>' . PHP_EOL;
+
+?>
+
+<?php
+
+$texto = <<<CHAVE
+Aqui nesta area vc pode
+escrever textos
+com muitas linhas.
+CHAVE;
+
+echo $texto
+?>
