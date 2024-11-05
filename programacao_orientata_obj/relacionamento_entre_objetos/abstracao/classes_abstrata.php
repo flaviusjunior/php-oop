50219e517f6b3ebb9a0f50806f99b4438362226d
Flávio Júnior <flaviojunior9400@gmail.com>
2024-11-05 13:52:54 -0300

    Abstrair, separar, tratar isoladamente

---
+++ programacao_orientata_obj/relacionamento_entre_objetos/abstracao/classes_abstrata.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'relacionamento_entre_objetos/heranca/conta_poupanca.php';
+
+//vai dar erro
+$conta = new Conta;
+
+?>
---
+++ programacao_orientata_obj/relacionamento_entre_objetos/abstracao/classes_final.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'relacionamento_entre_objetos/heranca/conta.php';
+require_once 'relacionamento_entre_objetos/heranca/conta_poupanca.php';
+
+// Vai dar erro
+class ContaPoupancaUniversitaria extends ContaPoupanca {
+
+}
+?>
